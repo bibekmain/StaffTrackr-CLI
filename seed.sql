22c1bbbfbd6999ed6aaa4d59661a70c9fc7663c9
BibekM <bibekmbkb@gmail.com>
2023-08-16 10:22:35 -0700

    init database setup, creating columns and rows

---
+++ seed.sql
@@ -0,0 +1,13 @@
+USE employee_db;
+
+INSERT INTO department (name) VALUES
+("Sales"),
+("Production");
+
+INSERT INTO role (title, salary, department_id) VALUES
+("Web Developer", 100000, 2),
+("Salesman", 70000, 1);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
+("Jim", "Halpert", 2, 2),
+("Dwight", "Schrute", 2, 2);
